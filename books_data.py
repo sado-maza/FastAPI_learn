286f32d96c871f5f219cc666325c7a7a399b63d1
Sado <sadist9395@gmail.com>
2026-01-23 10:09:39 +0300

    lesson2

---
+++ books_data.py
@@ -0,0 +1,17 @@
+data = [
+    {"id":1,
+     "name":"Гари Потер",
+     "author":"Роулинк",
+     "number_of_pages":"500",
+     "year_published":"2001"
+     },
+
+    {"id":2,
+     "name":"как стать меллеонером",
+     "author":"скала",
+     "number_of_pages":"20",
+     "year_published":"1905"
+     },
+
+
+]
--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from books_data import data
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return "hello ёпта"
+@app.get("/books", tags=["books"], summary="get all books")
+def get_books():
+    return data
+
+
+
+
+
+
+@app.get("/books/{book_id}", tags=["books"], summary="get one book by id")
+def get_book(book_id: int):
+    for book in data:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="book not found")
+
+
+
+
+
+
+class NewBook(BaseModel):
+    title: str
+    author: str
+    number_of_pages: int
+    year_published: str
+
+
+@app.post("/books", tags=["books"], summary="create new book")
+def add_book(new_book: NewBook):
+    data.append({
+        "id": data[-1]["id"]+1,
+        "title": new_book.title,
+        "author": new_book.author,
+        "number_of_pages": new_book.number_of_pages,
+        "year_published": new_book.year_published
+    })
+    return data[-1]
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
+
