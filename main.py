f82111ce869357171ef16a64d62b048d63dd5e8d
Sado <sadist9395@gmail.com>
2026-01-23 08:41:05 +0300

    lesson1

---
+++ main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "hello ёпта"
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
